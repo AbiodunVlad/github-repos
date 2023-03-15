761b1c7ac09884522ab732912b443b13cc29f7a3
AbiodunVlad <omonijoabiodun@gmail.com>
2023-03-15 16:24:42 +0100

    vue repo view

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/demo/rest/get-github-repo-using-vue/"
+      : "/",
+};
