74fcb5f2c4078e0d47608a17e7e5b82d5e6dc97a
AbiodunVlad <omonijoabiodun@gmail.com>
2023-03-15 21:35:30 +0100

    another commit

--- src/components/repositories.vue
+++ src/components/repositories.vue
@@ -1,37 +1,6 @@
 <template>
-  <div class="mb-6">
-    <section class="hero is-medium is-primary is-bold mb-6">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">Get GitHub Repositories</h1>
-          <h2 class="subtitle">using Vue JS, REST API &amp; Bulma</h2>
-        </div>
-      </div>
-    </section>
-    <div class="container">
-      <div class="table-container">
-        <table class="table is-bordered is-striped is-hoverable is-fullwidth">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>URL</th>
-              <th>Language</th>
-              <th>Login</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="repo in repos" v-bind:key="repo.id">
-              <td>{{ repo.id }}</td>
-              <td>{{ repo.name }}</td>
-              <td>{{ repo.html_url }}</td>
-              <td>{{ repo.language }}</td>
-              <td>{{ repo.owner.login }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+  <div>
+    <h1 class="title">Get GitHub Repositories</h1>
   </div>
 </template>
 
